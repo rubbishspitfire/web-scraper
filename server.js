4096db0babc1d73e303e2cddc4355157e89540c1
Patience Condell <condellp@gmail.com>
2020-02-05 03:37:44 -0500

    adding server.js and both public/models folders & files

---
+++ server.js
@@ -0,0 +1,91 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+
+var axios = require("axios");
+var cheerio = require("cheerio");
+
+
+var db = require("./models");
+
+var PORT = 3000;
+
+var app = express();
+
+
+app.use(logger("dev"));
+
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(express.static("public"));
+
+mongoose.Promise = Promise;
+mongoose.connect("mongodb://localhost/week18Populater", {
+    useMongoClient: true
+});
+
+
+app.post("/scraper", function(req, res) {
+    axios.get("http://www.echojs.com/").then(function(response) {
+        var $ = cheerio.load(response.data);
+
+        $("article h2").each(function(i, element) {
+            var result = {};
+
+            result.title = $(this)
+            .children("a")
+            .text();
+            result.link = $(this)
+            .children("a")
+            .attr("href");
+
+            db.Article.create(result)
+            .then(function(dbArticle) {
+                console.log(dbArticle);
+            })
+            .catch(function(err) {
+                return res.json(err);
+            });
+        });
+
+        res.send("Scape Complete");
+    });
+});
+
+app.get("/articles", function(req, res) {
+    db.Article.find({})
+    .then(function(dbArticle) {
+        res.join(dbArticle);
+    })
+    .catch(function(err) {
+        res.json(err);
+    });
+});
+
+app.get("/articles/:id", function(req, res) {
+    db.Article.findOne({ _id: req.params.id })
+    .populate("note")
+    .then(function(dbArticle) {
+        res.json(dbArticle);
+    })
+    .catch(function(err) {
+        res.json(err);
+    });
+});
+
+app.get("/articles/:id", function(req, res) {
+    db.Note.create(req.body)
+    .then(function(dbNote) {
+        return db.Article.update({ _id: req.params.id }, {$push: { note:dbNote }});
+    })
+    .then(function(dbArticle) {
+        res.json(dbArticle);
+    })
+    .catch(function(err) {
+        res.json(err);
+    });
+});
+
+app.listen(PORT, function() {
+    console.log("App running on port " + PORT + "!");
+});
